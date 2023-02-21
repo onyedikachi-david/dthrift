c0f8635e058dad11b4efa13472d81856c2fa6f28
David <davidanyatonwu@gmail.com>
2023-02-21 01:39:52 +0100

    Added: README and more documentation and others

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-# Cooking something on Concordium
+# Tanda Smart Contract
+
+The Tanda smart contract is a blockchain-based implementation of a traditional financial model commonly known as Tanda, Osusu, or rotating savings and credit association (ROSCA).
+
+## Overview
+
+A Tanda is a group of individuals who agree to contribute a fixed amount of money at regular intervals. Each member of the group takes turns receiving the pooled sum until every member has received it. The Tanda is complete when all members have received their share.
+
+The Tanda smart contract automates the process of managing and distributing funds within a Tanda group. Members can join the Tanda, make their contributions, and receive their payouts automatically through the smart contract.
+
+## How it works
+
+The Tanda smart contract allows users to create a Tanda group by specifying the number of members, the amount to be contributed, and the payout interval. Once the group is created, users can join the Tanda group by paying their contribution amount. When all members have joined, the first member in the queue receives the payout, and the cycle repeats until all members have received their payout.
+
+The Tanda smart contract uses a queue to keep track of the order in which members receive their payout. Once a member receives their payout, they are moved to the back of the queue. If a member misses a payment, they are removed from the queue and cannot receive their payout until they catch up on their payments.The Tanda smart contract allows users to create a Tanda group by specifying the number of members, the amount to be contributed, and the payout interval. Once the group is created, users can join the Tanda group by paying their contribution amount. When all members have joined, the first member in the queue receives the payout, and the cycle repeats until all members have received their payout.
+
+The Tanda smart contract uses a queue to keep track of the order in which members receive their payout. Once a member receives their payout, they are moved to the back of the queue. If a member misses a payment, they are removed from the queue and cannot receive their payout until they catch up on their payments.
+## Features
+
+The Tanda smart contract includes the following features:
+
+- Members can join the Tanda and make their contributions.
+- The smart contract keeps track of the contributions made by each member.
+- The smart contract calculates the payout amount for each member based on the Tanda's rules.
+- Members can withdraw their payouts from the smart contract.
+- The Tanda can be configured with custom rules, such as the contribution amount, payout frequency, and payout order.
+
+## Usage
+
+To use the Tanda smart contract, deploy it to a blockchain network that supports smart contracts. Then, members of the Tanda group can interact with the smart contract through a blockchain client or web interface.
+
+To join the Tanda, a member sends a transaction to the smart contract with their contribution amount. The smart contract adds the contribution to the member's account and tracks their participation in the Tanda.
+
+When it's time for a member to receive their payout, the smart contract calculates the payout amount and sends it to the member's account. The member can then withdraw their payout from the smart contract.
+
+The Tanda smart contract can be customized to fit the needs of a specific Tanda group. The administrator of the smart contract can configure the contribution amount, payout frequency, and payout order according to the group's rules.
+
+## Development
+
+The Tanda smart contract is implemented in Rust using the concordium libraries. To develop the smart contract, you'll need to have Rust and the concordium cargo library installed on your machine.
+
+To build the smart contract, run the following command:
+
+To deploy the smart contract to the blockchain node for testing, run the following command:
+
+## Potential Future Enhancements
+Here are some potential future enhancements to the Tanda smart contract:
+
+Support for different payout schedules (e.g. bi-weekly instead of monthly).
+Ability to specify a different order for members to receive payouts (e.g. reverse order instead of sequential order).
+Support for partial contributions (e.g. allow members to contribute less than the full cycle amount).
+
+## License
+
+This smart contract is licensed under the MIT License. See the `LICENSE` file for more information.
--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,86 @@
-//! # A Concordium V1 smart contract
+//! # Implementation of an Osusu, Tanda, or Informal Loan Club Smart Contract
+//!
+//! Members can join the Osusu, Tanda, or Informal Loan Club by invoking
+//! the join function and sending the specified amount in the specified currency.
+//!
+//! The smart contract keeps track of the contributions made by each member,
+//! and the total amount of contributions. Each member is entitled to receive
+//! a payout from the club's total contributions in each payout cycle.
+//! The payout cycle is defined when initializing the contract and is set in days or weeks.
+//!
+//! Members can withdraw their contributions before the end of the payout
+//! cycle, but they forfeit the right to receive future payouts from the club.
+//!
+//! At the end of each payout cycle, the smart contract initiates a payout
+//! to a member based on a rotating algorithm that determines the next member
+//! in line to receive a payout. The amount of the payout is equal to the
+//! total amount of contributions divided by the number of payout cycles
+//! and the number of members in the club.
+//!
+//! After the final payout cycle, any member can finalize the club
+//! and receive the remaining balance of the club's contributions.
+//! The smart contract transfers the balance to the account of the
+//! member who finalizes the club. This can be done only once.
+//!
+//! Terminology: `Members` are the individuals who join the club
+//! by invoking the join function and sending the specified amount
+//! in the specified currency. `Contract` instances are created by
+//! deploying a smart contract module and initializing it with the
+//! payout cycle, amount of payout, and other parameters.
+
 use concordium_std::*;
 use core::fmt::Debug;
 
-#[derive(Serialize, SchemaType, Clone, Copy)]
-enum PurseState {
-    OPEN,
-    CLOSE
+#[derive(Serialize, SchemaType, Clone, Copy, Debug)]
+pub enum TandaState {
+    /// The Tanda is accepting new members.
+    Open,
+    /// The Tanda has reached its maximum number of members and is no longer
+    /// accepting new members.
+    Closed,
+    /// The Tanda has not yet started the payout cycles.
+    Pending,
+    /// The Tanda is in progress and is currently paying out to members.
+    InProgress,
+    /// The Tanda has completed all payout cycles and is ready for finalization.
+    Completed,
 }
 /// Your smart contract state.
-#[derive(Serialize, SchemaType, Clone)]
+// pub struct State {
+//     // Your state
+//     name: String,
+//     description: String,
+//     creator: AccountAddress,
+//     members: Vec<AccountAddress>,
+//     amount: u128,
+//     collateral: u128,
+//     max_members: u32,
+//     purse_state: PurseState,
+//     time_created: Timestamp,
+//     time_interval: Timestamp,
+//     end_time: Timestamp,
+// }
+#[derive(Debug, Serialize, SchemaType, Clone)]
 pub struct State {
-    // Your state
-    description: String,
-    members: Vec<AccountAddress>,
-    purseState: PurseState,
-    time_created: Timestamp,
-    time_interval: Timestamp,
+    /// State of the Tanda
+    tanda_state: TandaState,
+    /// The list of members who have joined the Tanda
+    members: Vec<(AccountAddress, u64)>,
+    /// The amount of money each member contributes to the Tanda
+    contribution_amount: u64,
+    /// The total amount of contributions made by all members
+    total_contributions: u64,
+    /// The payout cycle for the Tanda
+    payout_cycle: u64,
+    /// The current payout cycle
+    current_cycle: u64,
+    /// The time when the Tanda started or will start
+    start_time: Timestamp,
+    /// The time when the Tanda will be finalized
+    end_time: Timestamp,
+    /// The member who is next in line to receive a payout
+    next_receiver: Option<AccountAddress>,
 }
-
 /// Your smart contract errors.
 #[derive(Debug, PartialEq, Eq, Reject, Serial, SchemaType)]
 enum Error {
@@ -28,19 +91,48 @@ enum Error {
     YourError,
 }
 
-struct InitParameter {
+#[derive(Serialize, SchemaType, Clone)]
+// struct InitParameter {
+//     name: String,
+//     description: String,
+//     creator: AccountAddress,
+//     amount: u128,
+//     collateral: u128,
+//     max_members: u32,
+//     time_interval: Timestamp,
+// }
 
+struct InitParameter {
+    /// The amount of money each member contributes to the Tanda
+    contribution_amount: u64,
+    /// The payout cycle for the Tanda
+    payout_cycle: u64,
+    /// The time when the Tanda will start using the RFC 3339 format (https://tools.ietf.org/html/rfc3339)
+    start_time: Timestamp,
+    /// The time when the Tanda will be finalized using the RFC 3339 format (https://tools.ietf.org/html/rfc3339)
+    end_time: Timestamp,
+    /// The penalty amount for missed payments
+    penalty_amount: u64,
 }
 
 /// Init function that creates a new smart contract.
-#[init(contract = "dthrift")]
+#[init(contract = "dthrift", parameter = "InitParameter")]
 fn init<S: HasStateApi>(
-    _ctx: &impl HasInitContext,
+    ctx: &impl HasInitContext,
     _state_builder: &mut StateBuilder<S>,
 ) -> InitResult<State> {
     // Your code
+    let param: InitParameter = ctx.parameter_cursor().get()?;
+    let acc = match ctx.sender() {
+        Address::Account(acc) => acc,
+        Address::Contract(_) => return Err(ContractError::ContractVoter),
+    };
 
-    Ok(State {})
+    Ok(State {
+        name: param.name,
+        description: param.description,
+        creator: acc,
+    })
 }
 
 /// Receive function. The input parameter is the boolean variable `throw_error`.
@@ -77,6 +169,4 @@ fn view<'b, S: HasStateApi>(
 }
 
 #[concordium_cfg_test]
-mod tests {
- 
-}
+mod tests {}
